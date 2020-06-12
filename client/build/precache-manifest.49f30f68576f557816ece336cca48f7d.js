1f12503d619d2bb3de350351f71315553fe52eb7
Sunny Gupta <gupta.sunny2016@gmail.com>
2020-06-12 14:44:19 +0545

    commit

---
+++ client/build/precache-manifest.49f30f68576f557816ece336cca48f7d.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "483af4a72b5cd451447e777b4c1558d8",
+    "url": "/MERN_EXERCISE_TRACKER/index.html"
+  },
+  {
+    "revision": "c427f068500a98f29c66",
+    "url": "/MERN_EXERCISE_TRACKER/static/css/2.eb99f8f5.chunk.css"
+  },
+  {
+    "revision": "c427f068500a98f29c66",
+    "url": "/MERN_EXERCISE_TRACKER/static/js/2.02cec66f.chunk.js"
+  },
+  {
+    "revision": "4414b5bbcc6f001f674a",
+    "url": "/MERN_EXERCISE_TRACKER/static/js/main.3294a687.chunk.js"
+  },
+  {
+    "revision": "c9b49b34548388a0f257",
+    "url": "/MERN_EXERCISE_TRACKER/static/js/runtime-main.51f3bb97.js"
+  }
+]);
--- server.js
+++ server.js
@@ -10,7 +10,9 @@ const port = process.env.PORT || 5000;
 app.use(cors());
 app.use(express.json());
 
-app.use(express.static(path.join(__dirname, "client", "build")));
+if (process.env.NODE_ENV === 'production'){
+	app.use(express.static('client/build'));
+}
 
 const uri = process.env.ATLAS_URI;
 mongoose.connect(uri, { useNewUrlParser: true, useCreateIndex: true}
@@ -29,6 +31,7 @@ app.use('/users', usersRouter);
 app.get("*", (req, res) => {
     res.sendFile(path.join(__dirname, "client", "build", "index.html"));
 });
+
 
 
 app.listen(port, () => {
